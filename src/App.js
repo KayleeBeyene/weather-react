eccf3487cd129674d38fd83f179984e869dcbd7a
KayleeBeyene <100857554+KayleeBeyene@users.noreply.github.com>
2022-03-16 22:13:45 -0600

    Imported Bootsrtap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import "./App.css";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Weather />
-      <p>
-        <a
-          href="https://github.com/KayleeBeyene/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source
-        </a>
-        {""}
-        code
-      </p>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <p>
+          <a
+            href="https://github.com/KayleeBeyene/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source
+          </a>{" "}
+          code by Kaylee Beyene
+        </p>
+      </div>
     </div>
   );
 }
